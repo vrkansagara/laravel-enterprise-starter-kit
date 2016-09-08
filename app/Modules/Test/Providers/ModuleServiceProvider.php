259d2b1ee4977d80a0a0a21872b42a5354474141
Vallabh Kansagara <vrkansagara@gmail.com>
2016-09-08 15:01:58 +0530

    project init commit

---
+++ app/Modules/Test/Providers/ModuleServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Modules\Test\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'test');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'test');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
