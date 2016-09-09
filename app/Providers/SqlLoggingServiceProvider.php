05d37ea4a3ae0a8161cc61c3695c70324a69446f
Vallabh Kansagara <vrkansagara@gmail.com>
2016-09-09 17:44:09 +0530

    sql-loggin file added for a while being

---
+++ app/Providers/SqlLoggingServiceProvider.php
@@ -0,0 +1,63 @@
+<?php
+namespace Libern\SqlLogging;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Event;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+class SqlLoggingServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/sql-logging.php';
+        if (function_exists('config_path')) {
+            $publishPath = config_path('sql-logging.php');
+        } else {
+            $publishPath = base_path('config/sql-logging.php');
+        }
+        $this->publishes([$configPath => $publishPath], 'config');
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $configPath = __DIR__ . '/../config/sql-logging.php';
+        $this->mergeConfigFrom($configPath, 'sql-logging');
+        if (config('sql-logging.log', false)) {
+            Event::listen('illuminate.query',
+                function ($query, $bindings, $time) {
+                    $data = compact('bindings', 'time');
+                    // Format binding data for sql insertion
+                    foreach ($bindings as $i => $binding) {
+                        if ($binding instanceof \DateTime) {
+                            $bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+                        } else if (is_string($binding)) {
+                            $bindings[$i] = "'$binding'";
+                        }
+                    }
+                    // Insert bindings into query
+                    $query = str_replace(array('%', '?'), array('%%', '%s'), $query);
+                    $query = vsprintf($query, $bindings);
+                    $log = new Logger('sql');
+                    $log->pushHandler(new StreamHandler(storage_path() . '/logs/sql-' . date('Y-m-d') . '.log',
+                        Logger::INFO));
+                    // if log request data
+                    if (config('sql-logging.log_request', false)) {
+                        $data['request_path'] = Request::path();
+                        $data['request_method'] = Request::method();
+                        $data['request_data'] = Request::all();
+                    }
+                    // add records to the log
+                    $log->addInfo($query, $data);
+                });
+        }
+    }
+}
